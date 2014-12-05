367a3ffa35667b3e13a0176fe9faaf2347cdf9a5
tedwardmah <ted.mahoney22@gmail.com>
2014-12-05 16:52:38 -0500

    Modify tester.rb to oput json? file, pull some historical data

--- tester.rb
+++ tester.rb
@@ -1,11 +1,17 @@
 require './gtfs-realtime.pb.rb'
 require './nyct-subway.pb.rb'
 require 'pry'
+require 'json'
 
-serialized_string = File.read('./mtafeed')
+serialized_string = File.read('./historic_gtfs/gtfs-2014-09-17-00-01')
 
 transit_realtime = TransitRealtime::FeedMessage.parse(serialized_string)
 transit_realtime = transit_realtime.to_hash
 
-binding.pry
+f = File.open("historic_data_sample.json", "a+")
+f.write(transit_realtime.to_json)
+f.close
+
+
+# binding.pry
 
