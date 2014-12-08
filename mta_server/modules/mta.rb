f22c3988a8ceb585ec5e0131945789c68daef6e7
tedwardmah <ted.mahoney22@gmail.com>
2014-12-07 23:22:52 -0500

    mta module can determine the more recent feed

--- mta_server/modules/mta.rb
+++ mta_server/modules/mta.rb
@@ -2,7 +2,14 @@ module MTA
   class FeedParser
 
     def self.feed
-      'Feedz'
+      filenames = []
+      Dir.glob('./mta_assets/feeds/*/*.json') do |file|
+        filenames << [File.basename(file), File.dirname(file)]
+      end
+      filenames.sort! do |fileX, fileY|
+        Time.parse(fileX[0].chomp('_realtime.json').gsub!('_', ' ').gsub!('.', ':')) <=> Time.parse(fileY[0].chomp('_realtime.json').gsub!('_', ' ').gsub!('.', ':'))
+      end
+      binding.pry
     end
 
   end
