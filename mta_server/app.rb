0f09e3dff9a6235934efff0e7edf310da9cd63cb
tedwardmah <ted.mahoney22@gmail.com>
2014-12-07 20:13:54 -0500

    adds httparty and json to gemfile

--- mta_server/app.rb
+++ mta_server/app.rb
@@ -4,3 +4,8 @@ require './connection'
 ROOT_PATH = Dir.pwd
 Dir[ROOT_PATH+"/lib/*.rb"].each{ |file| require file }
 Dir[ROOT_PATH+"/helpers/*.rb"].each{ |file| require file }
+
+
+get '/api/update' do
+
+end
