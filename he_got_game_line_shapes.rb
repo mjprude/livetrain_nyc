764a09b5fb88bd7bdf400f2e97a1cb0124d91e1a
mjprude <michaelprude@gmail.com>
2014-12-12 10:04:23 -0500

    add methods for breaking up line shape by stop currently works for south-bound 1 trains

--- he_got_game_line_shapes.rb
+++ he_got_game_line_shapes.rb
@@ -18,8 +18,20 @@ def route
   data = JSON.parse(route)
 end
 
-def get_stations_by_line(line)
-  stops.map { |stop| stop['stops'].keys[0][0] == line.to_s ? stop['stops'].values : nil }.compact
+def get_stops_by_line(line)
+  all_stops = stops.map do |stop|
+    if stop['stops'].keys[0][0] == line.to_s
+      if stop['stops'].length > 1
+        stop_in_line = stop['stops'].select{|k, v| k[0] == line.to_s}
+        stop_in_line.values[0].reverse!
+        stop_in_line
+      else
+        stop['stops'].values[0].reverse!
+        stop['stops']
+      end
+    end
+  end.compact
+  all_stops.sort_by { |stop| stop.keys[0][1..2].to_i }
 end
 
 def get_shape_by_line(line)
@@ -27,68 +39,72 @@ def get_shape_by_line(line)
     if feature['properties']['route_id'] == line.to_s
       feature['geometry']['coordinates']
     end
-  end.compact
+  end.compact.flatten(2).reverse
 end
+# stop = [-73.899, 40.889248000933826]
+# # stop = [ -73.918822, 40.864621 ]
 
-stop = [ -73.918822, 40.864621 ]
-
-shape = get_shape_by_line('1').flatten(2)
+shape = get_shape_by_line('1')
 
 def distance_between(point1, point2)
-  (point1[1] - point2[1]).abs + (point1[0] - point2[0]).abs
+  begin
+    (point1[1] - point2[1]).abs + (point1[0] - point2[0]).abs
+  rescue
+    puts point1
+    puts point2
+  end
 end
 
-def insert_stop_into_shape(shape, stop)
+def subdivide_shape_by_stop(shape, stop)
+  stop_name, stop_point = stop.first
   # line = line.to_s
   # stations = get_stations_by_line(line).flatten(1)
-  shape.include?(stop) && return 'already got it'
-  closest_value = shape.min_by{ |point| distance_between(point, stop) }
-  closest_index = shape.index(closest_value)
-
-  stop_distance = distance_between(closest_value, stop)
-
-  if closest_index != shape.length
-    point_south = shape[closest_index + 1]
-
-    point_distance = distance_between(closest_value, point_south)
-
-    stop_distance > point_distance ? shape.insert(closest_index, stop) : shape.insert(closest_index + 1, stop)
-
+  if shape.include?(stop_point)
+    stop_index = shape.index(stop_point)
+    shape.insert(stop_index, stop_point)
+    stop[stop_name] = shape.shift(stop_index+1)
   else
-    point_distance = distance_between(closest_value, shape[closest_index -1])
-    stop_distance > point_distance ? shape.insert(-1, stop) : shape.insert(closest_index, stop)
+
+    closest_value = shape.min_by{ |point| distance_between(point, stop_point) }
+    closest_index = shape.index(closest_value)
+
+    if closest_index != shape.length - 1 && closest_index != 0
+      point_north = shape[closest_index + 1]
+      stop_distance = distance_between(stop_point, point_north)
+
+      point_distance = distance_between(closest_value, point_north)
+      if stop_distance > point_distance
+        shape.insert(closest_index, stop_point, stop_point)
+        stop[stop_name] = shape.shift(closest_index + 1)
+      else
+        shape.insert(closest_index + 1, stop_point, stop_point)
+        stop[stop_name] = shape.shift(closest_index + 2)
+      end
+
+    elsif closest_index
+      point_south = shape[closest_index - 1]
+      stop_distance = distance_between(stop_point, point_south)
+      point_distance = distance_between(closest_value, point_south)
+# add to end or replace endpoint
+      stop_distance > point_distance ? shape.insert(-1, stop_point) : shape[-1] = stop_point
+
+      stop[stop_name] = shape
+    end
   end
-
-    # direction = [(shape[closest_index + 1][0] <=> closest_value[0]]), (shape[closest_index + 1][1] <=> closest_value[1]])]
-    # stop_direction = [(stop[0] <=> closest_value[0]), (stop[1] <=> closest_value[1])]
-
-    # line_east? = (shape[closest_index + 1][0] - closest_value[0]) > 0
-    # line_south? = (shape[closest_index + 1][1] - closest_value[1]) < 0
-    # stop_east? = (stop[0] - closest_value[0]) > 0
-    # stop_south? = (stop[1] - closest_value[1]) < 0
-
-
-  # result = shape.select{ |point| (point[1] - stop[1]).abs < x }
-
-  # if result.length < 2
-  #   insert_stop_into_shape(shape, stop, x+0.001)
-  # elsif result.length > 2
-  #   insert_stop_into_shape(shape, stop, x-0.001)
-  # else
-  #   binding.pry
-  #   return result
-  # end
-
-  # stations.map do |station|
-  #   shape.assoc(station[1]) || shape.rassoc(station[0])
-  # end
-
-  # shape.map do |point|
-  #   stations.select { |stop| stop[0] == point[1] || stop[1] == point[0] }
-  # end
 end
 
+shape = get_shape_by_line(1)
+all_stops = get_stops_by_line(1)
 
+all_stops.each{ |stop| subdivide_shape_by_stop(shape, stop)}
 
+f = File.open('one_stops_south.txt', 'w+')
+f.write '['
+all_stops.each{ |stop|
+    f.write stop
+  f << ",\n"
+}
+f.write ']'
+f.close
 
 binding.pry
