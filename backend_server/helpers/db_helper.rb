56ea31b377cc957f1dbeb1b25148b38f0d4286e7
tedwardmah <ted.mahoney22@gmail.com>
2014-12-17 17:00:49 -0500

    update_json helper filters out trips in the distant future

--- backend_server/helpers/db_helper.rb
+++ backend_server/helpers/db_helper.rb
@@ -6,35 +6,36 @@ def update_json
   route_obj = {}
 
   Trip.where(route: '1').each do |trip|
+    if trip.start_time - Time.now < 60
+      future_stops = trip.stops.where('arrival_time > ?',  Time.now.to_i).order('arrival_time ASC').first
+      last_stop = trip.stops.where('departure_time < ?',  Time.now.to_i).order('departure_time DESC').first
+      last_stop ||= trip.stops.where('departure_time > ?',  Time.now.to_i).order('departure_time ASC').first
+      stop1 = future_stops || last_stop
 
-    future_stops = trip.stops.where('arrival_time > ?',  Time.now.to_i).order('arrival_time ASC').first
-    last_stop = trip.stops.where('departure_time < ?',  Time.now.to_i).order('departure_time DESC').first
-    last_stop ||= trip.stops.where('departure_time > ?',  Time.now.to_i).order('departure_time ASC').first
-    stop1 = future_stops || last_stop
+      begin
+        if stop1 && last_stop
 
-    begin
-      if stop1 && last_stop
+          route_obj = {
+            trip_id: 't' + trip.mta_trip_id.gsub('.', '_'),
+            route: trip.route,
+            direction: trip.direction,
+            updated: trip.mta_timestamp,
 
-        route_obj = {
-          trip_id: 't' + trip.mta_trip_id.gsub('.', '_'),
-          route: trip.route,
-          direction: trip.direction,
-          updated: trip.mta_timestamp,
+            lastStop: last_stop.stop_id,
+            lastDeparture: last_stop.departure_time,
 
-          lastStop: last_stop.stop_id,
-          lastDeparture: last_stop.departure_time,
-
-          stop1: stop1.stop_id,
-          path1: Shapes.get_path(trip.route, last_stop.stop_id, stop1.stop_id),
-          arrival1: stop1.arrival_time,
-          departure1: stop1.departure_time,
-        }
+            stop1: stop1.stop_id,
+            path1: Shapes.get_path(trip.route, last_stop.stop_id, stop1.stop_id),
+            arrival1: stop1.arrival_time,
+            departure1: stop1.departure_time,
+          }
+        end
+      rescue Exception => e
+        e
+        binding.pry
       end
-    rescue Exception => e
-      e
-      binding.pry
+      all_trains << route_obj
     end
-    all_trains << route_obj
   end
   all_trains.to_json
 end
