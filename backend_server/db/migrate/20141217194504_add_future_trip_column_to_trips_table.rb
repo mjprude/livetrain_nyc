dfb5d7d870bb66928d3301ce75b130eef9e4eaa7
tedwardmah <ted.mahoney22@gmail.com>
2014-12-17 15:23:29 -0500

    deletes future_trip migration

--- backend_server/db/migrate/20141217194504_add_future_trip_column_to_trips_table.rb
+++
@@ -1,5 +0,0 @@
-class AddFutureTripColumnToTripsTable < ActiveRecord::Migration
-  def change
-    add_column :trips, :future_trip, :boolean
-  end
-end
--- backend_server/db/schema.rb
+++ backend_server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217194504) do
+ActiveRecord::Schema.define(version: 20141216004759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20141217194504) do
     t.integer  "last_stop_departure"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "future_trip"
   end
 
 end
