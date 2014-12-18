acfb26c145912c388bde35525fd2f8f0f78f8fdb
mjprude <michaelprude@gmail.com>
2014-12-18 12:24:47 -0500

    change connection to more Heroku-friendly format

--- backend_server/connection.rb
+++ backend_server/connection.rb
@@ -1,4 +1,6 @@
-ActiveRecord::Base.establish_connection({
-  adapter: 'postgresql',
-  database: 'mta_realtime'
-  })
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mta_realtime')
+
+# ActiveRecord::Base.establish_connection({
+#   adapter: 'postgresql',
+#   database: 'mta_realtime'
+#   })
