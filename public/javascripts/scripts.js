b082522240e91e3fa676fb0a1a07a094505b17b6
Ted Mahoney <ted.mahoney22@gmail.com>
2015-01-27 18:57:48 -0500

    eliminated station_countdown directory in preparation for additional backbone model

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -93,7 +93,8 @@ function getBounds(){
 
 function update() {
   $.ajax({
-    url: 'http://104.131.206.60/api/update',
+    // url: 'http://104.131.206.60/api/update',
+    url: 'http://localhost:3000/api/update',
     dataType: 'JSON',
     success: animate
   });
